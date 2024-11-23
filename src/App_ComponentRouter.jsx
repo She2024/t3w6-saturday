04fa0cedd297a2f877dca10499984011365bf51d
she2024 <bullybluedog@hotmail.com>
2024-11-23 11:16:55 +1000

    feat: nested route

--- src/App_ComponentRouter.jsx
+++ src/App_ComponentRouter.jsx
@@ -3,20 +3,25 @@ import './styles/App.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import TerminalAppProject from './pages/TerminalAppProject';
 import Navbar from './components/Navbar';
+import ProjectLayout from './pages/_templates/ProjectLayout';
 
 function App() {
   return (
     <div className="App">
       {/* Routes are declared within this component */}
-      
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         {/* Declare individual route components here */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/projects" element={<ProjectLayout />}>
+            <Route path='/projects/terminalApp' element={<TerminalAppProject />} />
+            <Route path='terminalApp' element={<TerminalAppProject />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
