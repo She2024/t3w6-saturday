de384f24da6fbe57dc72223d5026a32a63071f66
she2024 <bullybluedog@hotmail.com>
2024-11-23 10:58:22 +1000

    feat: added home buttton using navigation

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,10 @@
+import RedirectButton from "../components/RedirectButton";
 
 export default function AboutPage(){
     return(
         <div>
             <h1>This is my About Page.</h1>
+            <RedirectButton/>
         </div>
     )
 }
